37e38e67fbcdf32b159bb0b658753ac6f0eabd2a
Saitejamaddi <saitejamaddi123@gmail.com>
2026-05-18 17:58:39 +0530

    Changed Back Port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors       = require('cors');
 
 
 const app        = express();
-const PORT       = 8080;
+const PORT       = 7000;
 const CLIENT_ID  = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
